4fc9a9a7fc52c8ed204c882bfe5845a0cddba0ec
Mark Chang <ak0922747401@gmail.com>
2020-05-29 21:38:26 +0800

    add kafka readStream source

--- data_stream.py
+++ data_stream.py
@@ -1,13 +1,32 @@
 import logging
-import json
+import os
 from pyspark.sql import SparkSession
-from pyspark.sql.types import *
+from pyspark.sql.types import (
+   StructType, StructField, StringType, IntegerType, TimestampType
+)
 import pyspark.sql.functions as psf
 
 
+os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5 pyspark-shell' # noqa
+
 # TODO Create a schema for incoming resources
 schema = StructType([
+    StructField('crime_id', IntegerType(), True),
+    StructField('original_crime_type_name', StringType(), True),
+    StructField('report_date', TimestampType(), True),
+    StructField('call_date', TimestampType(), True),
+    StructField('offense_date', TimestampType(), True),
+    StructField('call_time', StringType(), True),
+    StructField('call_date_time', StringType(), True),
+    StructField('disposition', StringType(), True),
+    StructField('address', StringType(), True),
+    StructField('city', StringType(), True),
+    StructField('state', StringType(), True),
+    StructField('agency_id', IntegerType(), True),
+    StructField('address_type', StringType(), True),
+    StructField('common_location', StringType(), True)
 ])
+
 
 def run_spark_job(spark):
 
@@ -16,23 +35,32 @@ def run_spark_job(spark):
     # set up correct bootstrap server and port
     df = spark \
         .readStream \
+        .format("kafka") \
+        .option("subscribe", "sf.crime.stat.topic") \
+        .option("startingOffsets", "earliest") \
+        .option("stopGracefullyOnShutdown", "true") \
+        .option("maxOffsetsPerTrigger", 200) \
+        .option("kafka.bootstrap.servers", "127.0.0.1:9092") \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
     # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
 
-    service_table = kafka_df\
+    service_table = kafka_df \
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
         .select("DF.*")
 
+    service_table.printSchema()
+
     # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    distinct_table = ''
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = ''
 
     # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
     # TODO write output stream
@@ -46,15 +74,15 @@ def run_spark_job(spark):
     radio_code_json_filepath = ""
     radio_code_df = spark.read.json(radio_code_json_filepath)
 
-    # clean up your data so that the column names match on radio_code_df and agg_df
+    # clean up your data so that the column names
+    # match on radio_code_df and agg_df
     # we will want to join on the disposition code
 
     # TODO rename disposition_code column to disposition
-    radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
+    radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition") # noqa
 
     # TODO join on disposition column
-    join_query = agg_df.
-
+    join_query = agg_df \
 
     join_query.awaitTermination()
 
